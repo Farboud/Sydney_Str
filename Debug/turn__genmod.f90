d8fedabde765c35cbba4b1fc14875c09fb9699dd
Farboud <32154184+Farboud@users.noreply.github.com>
2017-10-05 12:05:34 +0330

    Add files via upload

---
+++ Debug/turn__genmod.f90
@@ -0,0 +1,10 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Thu Jul 03 11:31:41 2014
+        MODULE TURN__genmod
+          INTERFACE 
+            SUBROUTINE TURN(C,S,T)
+              REAL(KIND=8) :: C
+              REAL(KIND=8) :: S
+              REAL(KIND=8) :: T(6,6)
+            END SUBROUTINE TURN
+          END INTERFACE 
+        END MODULE TURN__genmod
